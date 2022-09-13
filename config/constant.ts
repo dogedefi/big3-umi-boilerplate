e2d0aa0ea82468e05970d3b239742539b0597017
Benhaben <web3monkey@proton.me>
2022-09-13 12:21:40 +0800

    load variables from dotenv

---
+++ config/constant.ts
@@ -0,0 +1,5 @@
+import './loadEnv';
+
+export const PROJECT = process.env.PROJECT;
+export const MOBILE_DESIGN_DRAFT_WIDTH = process.env.MOBILE_DESIGN_DRAFT_WIDTH;
+export const PRIMARY_COLOR = `#${process.env.PRIMARY_COLOR || '503e9d'}`;
